84e6741abf30710eaeec03096084c7b339171f2f
RonanCanales <canalesronan@yahoo.ie>
2025-11-14 19:03:16 +0000

    Fixes

--- GameJamGame/Assets/Health.cs
+++ GameJamGame/Assets/Health.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Health : MonoBehaviour
@@ -9,11 +10,24 @@ public class Health : MonoBehaviour
     [SerializeField] private int currencyWorth = 50;
 
     private bool isDestroyed = false;
+    private int maxHitPoints;
+
+    private void Start()
+    {
+        maxHitPoints = hitPoints;
+    }
 
     public void TakeDamage(int dmg)
     {
+        if (isDestroyed) return;
         hitPoints -= dmg;
+        hitPoints = Mathf.Max(0, hitPoints);
 
+       /*  EnemyHealthBar enemyHealthBar = GetComponent<EnemyHealthBar>();
+        if (enemyHealthBar != null)
+        {
+            enemyHealthBar.UpdateHealthBar();
+        } */
         if (hitPoints <= 0 && !isDestroyed)
         {
             EnemySpawner.onEnemyDestroy.Invoke();
@@ -21,5 +35,24 @@ public class Health : MonoBehaviour
             isDestroyed = true;
             Destroy(gameObject);
         }
+        
+        /*  if (hitPoints <= 0 && !isDestroyed)
+        {
+            EnemySpawner.onEnemyDestroy.Invoke();
+            EnemyManager.main.IncreaseCurrency(currencyWorth);
+            isDestroyed = true;
+            Destroy(gameObject);
+        } */
     }
+
+    public int GetCurrentHealth()
+    {
+        return hitPoints;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHitPoints;
+    }
+
 }
